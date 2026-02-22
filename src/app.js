5f20eb627e2235a3d65af53a5c629c7d5694df66
Nouha-Tk <toukournouha@gmail.com>
2026-02-22 16:28:37 +0000

    Update Welcome message on main branch

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ let tasks = [
 ];
 
 app.get('/', (req, res) => {
-  res.json({ message: "DevOps Task Manager API is running... Lab1-test" });
+  res.json({ message: "Welcome from MAIN branch" });
 });
 
 app.get('/tasks', (req, res) => {
